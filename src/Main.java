7ce4f85b964a11c0c0dae87c73d96cc0c1fed2ff
Ramil <rdgalimov@yandex.ru>
2022-12-22 00:22:02 +0300

    тренировка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Иванов Иван Иванович", 1, 30000);
+        employees[1] = new Employee("Иванов Петр Сергеевич", 2, 31000);
+        employees[2] = new Employee("Сидоров Иван Сергеевич", 5, 39000);
+        employees[3] = new Employee("Сидоров Сергей Иванович", 3, 35500);
+        employees[4] = new Employee("Петрова Елена Ивановна", 4, 42500);
+        employees[5] = new Employee("Петрова Татьяна Сергеевна", 5, 32800);
+        employees[6] = new Employee("Петров Сергей Иванович", 2, 34300);
+        employees[7] = new Employee("Васильев Петр Иванович", 1, 27000);
+        employees[8] = new Employee("Захаров Павел Сергеевич", 3, 46500);
+        employees[9] = new Employee("Павлов Иван Сергеевич", 5, 41500);
+        System.out.println(Arrays.toString(employees));
+        Employee.printAllEmployees(employees);
+
+
+
+
+
+
+
+
+
+
         System.out.println("Домашнее задание");
         Author author = new Author("Алексей", "Толстой");
         Author author1 = new Author("Лев", "Толстой");
@@ -22,7 +47,6 @@ public class Main {
         System.out.println(book1);
         System.out.println(book.equals(book1));
         System.out.println(author.equals(author1));
-
 
     }
 }
